98bfb80ca4bccb45483df481a96606ef10caae52
khomoutov <khomoutov@9d8bde4f-0d4a-0410-8f4e-1563d1b116ea>
2008-04-04 15:03:52 +0000

    r15@jukebox (orig r227):  kostix | 2008-03-28 04:55:06 +0300  src/marshal.tcl:   * Implementation changed to provide for independed "marshaling list"     and values for its elements in MarshalList.   * Fixed logic of MarshalVariant -- now it's possible to have compound     and aggregate types marshaled in variants.   * Implemented processing of nested arrays in MarshalArray.   * Fixed checking of message size -- now the total size is checked,     as required by the spec.   * Generation of message serials is moved to MarshalHeader, it's fixed     to track wraparounds with prevention of serial being 0.   * MarshalMethodCall adjusted to match mentioned changed.   * Misc code rearrange.
     test.tcl: Deleted old tests, added new.

--- trunk/test.tcl
+++ trunk/test.tcl
@@ -2,30 +2,18 @@ set auto_path [linsert $auto_path 0 .]
 package require dbus
 
 set s ""
-if 0 {
-::dbus::MarshalByte s 64
-::dbus::MarshalString s AжопаA
-::dbus::MarshalDouble s 8.93e-2
-::dbus::MarshalBasicArray s INT32 {1 2 3 4}
-::dbus::MarshalArray s {INT32 2 {
-	{0x01 0x02 0x03 0x04}
-	{0x11 0x12 0x13 0x14}
-	{0x21 0x22 0x23 0x24}
-	{0x31 0x32 0x33 0x34}
-}}
-::dbus::MarshalArray s {STRUCT 1 {{INT32 0xAA STRING GabbaHey!}}}
-}
 
-#set in  {BYTE 64 INT32 0xDEADBEEF INT16 0xAABB}
-#set in  {BYTE 64 STRUCT {STRING ABCDE UINT32 0xDEADBEEF}}
-if 0 {
-set in  {ARRAY {1 STRUCT {
-	{STRING ABCDE UINT16 0xDEAD}
-	{STRING FGHIJ UINT16 0xAABB}
-}}}
-set out [::dbus::MarshalListTest $in]
-}
-set out [::dbus::MarshalMethodCall /org/Freedesktop/DBus Hello "" {}]
+set mlist {INT32 {} BYTE {} ARRAY {2 STRUCT {STRING {} INT16 {}}}}
+set out [::dbus::MarshalListTest $mlist {0xAABBCCDD 0xFF {
+	{{"foo" 0xDEAD} {"bar" 0xFAFA}} \
+	{{"abba" 0xCC99} {"gabba" 0xBB77}}}}]
+
+set out [::dbus::MarshalMethodCall \
+	org.freedesktop.DBus \
+	/org/freedesktop.DBus \
+	org.freedesktop.DBus \
+	Hello \
+	"" {}]
 
 set settings [fconfigure stdout]
 fconfigure stdout -translation binary
