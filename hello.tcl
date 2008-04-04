ba49c8eb3ae2c653101935704f3f5254a0a508cf
khomoutov <khomoutov@9d8bde4f-0d4a-0410-8f4e-1563d1b116ea>
2008-04-04 15:03:41 +0000

    r14@jukebox (orig r226):  kostix | 2008-03-26 04:16:47 +0300  dtest: Added a set of config files and wrapper script to run attached   dbus-monitor in verbose mode for debugging.
     src/marshal.tcl: Serial seed is changed to 1 since 0 is turned to be explicitly
      prohibited in the reference implementation.
     
     hello.tcl: Added sanitizing regexp for data read from the socket.
     
     reftest/dbus-unmarshal.c: Added "b" mode to dump file opening command.
     
     reftest/Makefile: Made use of special GNU Make target/sources variables.

--- hello.tcl
+++ hello.tcl
@@ -3,18 +3,19 @@ package require dbus
 
 if 1 {
 set chan [::dbus::connect /var/run/dbus/system_bus_socket -timeout 1000]
+#set chan [::dbus::connect /tmp/dbus_test -timeout 1000]
 puts Connected
 
 proc SockRead chan {
 	puts [info level 0]
 	set data [read $chan]
-	puts |$data|
 	if {[eof $chan]} {
 		close $chan
-		puts Closed
+		puts "Remote disconnect"
 		set ::forever 1
 		return
 	}
+	puts [regsub -all {[^\w/ -]} |$data| .]
 }
 fileevent $chan readable [list SockRead $chan]
 }
@@ -27,13 +28,13 @@ set out [::dbus::MarshalMethodCall \
 	"" {}]
 if 1 {
 set data [join $out ""]
-fconfigure $chan -translation binary
 puts -nonewline $chan $data
+
 set fd [open dump.bin w]
 fconfigure $fd -translation binary
 puts -nonewline $fd $data
 close $fd
-puts Sent
+puts "Sent Hello"
 
 vwait forever
 } else {
