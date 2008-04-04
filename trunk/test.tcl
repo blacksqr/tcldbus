86314f9f3084e040689b043b6b60fc310c18dc24
khomoutov <khomoutov@9d8bde4f-0d4a-0410-8f4e-1563d1b116ea>
2008-04-04 15:02:58 +0000

    r11@jukebox (orig r223):  kostix | 2008-03-25 00:36:18 +0300  src/marshal.tcl:   * Several improvements to the code for marshaling simple types.   * Attempt to implement marshaling of arrays.
     test.tcl: Misc changes to test new functionality.

--- trunk/test.tcl
+++ trunk/test.tcl
@@ -4,10 +4,18 @@ package require dbus
 set s ""
 ::dbus::MarshalByte s 64
 if 0 {
-::dbus::MarshalString s Foo
+::dbus::MarshalString s AжопаA
 ::dbus::MarshalDouble s 8.93e-2
+::dbus::MarshalBasicArray s INT32 {1 2 3 4}
 }
-::dbus::MarshalArray s INT16 {1 2 3 4}
-#::dbus::MarshalArray s INT32 {1 2 3 4}
+if 0 {
+::dbus::MarshalArray s {INT32 2 {
+	{0x01 0x02 0x03 0x04}
+	{0x11 0x12 0x13 0x14}
+	{0x21 0x22 0x23 0x24}
+	{0x31 0x32 0x33 0x34}
+}}
+}
+::dbus::MarshalArray s {STRUCT 1 {{INT32 0xAA STRING GabbaHey!}}}
 puts |$s|
 
