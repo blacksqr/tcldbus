98ed64930e59b1de037c349b365c2554b8e83b0a
khomoutov <khomoutov@9d8bde4f-0d4a-0410-8f4e-1563d1b116ea>
2008-04-04 15:01:38 +0000

    r4@jukebox (orig r216):  kostix | 2008-03-22 05:05:39 +0300  sample.tcl: Improved connection code:   * ceptcl is weakly required and [cep] is wrapped in a generic proc.   * Implemented custom connection timeout and async connection.   Authentication part is still missing.

--- sample.tcl
+++ sample.tcl
@@ -1,6 +1,113 @@
-package require ceptcl
 
-set path /var/run/dbus/system_bus_socket
+namespace eval ::dbus {
+	variable sysbus /var/run/dbus/system_bus_socket
+}
+
+proc ::dbus::Pop {varname {nth 0}} {
+	upvar $varname args
+	set r [lindex $args $nth]
+	set args [lreplace $args $nth $nth]
+	return $r
+}
+
+proc ::dbus::UnixDomainSocket {path args} {
+	package require ceptcl
+	interp alias {} ::dbus::UnixDomainSocket {} cep -domain local
+	eval UnixDomainSocket $args [list $path]
+}
+
+proc ::dbus::connect {dest args} {
+	set command ""
+	set timeout 0
+	set transport unix
+
+	while {[string match -* [set opt [Pop args]]]} {
+		switch -- $opt {
+			-command { set command [Pop args] }
+			-timeout { set timeout [Pop args] }
+			-transport { set transport [Pop args] }
+			default {
+				return -code error "Bad option \"$opt\":\
+					must be one of -command, -timeout or -transport"
+			}
+		}
+	}
+
+	switch -- $transport {
+		unix {
+			set sock [UnixDomainSocket $dest -async]
+		}
+		tcp {
+			foreach {host port} [split $dest :] break
+			if {$host == "" || $port == ""} {
+				return -code error "Bad TCP connection endpoint \"$dest\":\
+					must be in the form host:port"
+			}
+			set sock [socket -async $host $port]
+		}
+		default {
+			return -code error "Bad transport \"$transport\":\
+				must be unix or tcp"
+		}
+	}
+
+	variable $sock; upvar 0 $sock state
+
+	if {$timeout > 0} {
+		after $timeout [list [namespace current]::ProcessConnectTimeout $sock]
+	}
+	fileevent $sock writable [list ::dbus::ProcessConnectCompleted $sock]
+
+	if {$command != ""} {
+		set state(command) $command
+		return $sock
+	} 
+
+	vwait [namespace current]::${sock}(code)
+	set cmd [list return -code $state(code) $state(result)]
+	unset state
+	eval $cmd
+}
+
+proc ::dbus::ProcessConnectCompleted sock {
+	variable $sock; upvar 0 $sock state
+
+	after cancel [list [namespace current]::ProcessConnectTimeout $sock]
+	fileevent $sock writable {}
+
+	set err [fconfigure $sock -error]
+	if {$err == ""} {
+		set code   ok
+		set result $sock
+	} else {
+		close $sock
+		set code   error
+		set result $err
+	}
+
+	if {[info exists state(command)]} {
+		set cmd $state(command)
+		unset state
+		uplevel #0 [list $cmd $code $result]
+	} else {
+		set state(code)   $code
+		set state(result) $result
+	}
+}
+
+proc ::dbus::ProcessConnectTimeout sock {
+	variable $sock; upvar 0 $sock state
+
+	close $sock
+	set state(code)   error
+	set state(result) "connection timed out"
+}
+
+#set s [::dbus::connect $::dbus::sysbus -timeout 1000]
+#set s [::dbus::connect /var/run/kaboom]
+set s [::dbus::connect jabber.007spb.ru:80 -transport tcp]
+fconfigure $s -translation binary -buffering none -blocking no
+exit 0
 
 proc SockRead s {
 	if {[gets $s line] < 0} {
@@ -11,24 +118,5 @@ proc SockRead s {
 
 	puts $line
 }
-
-set s [cep -domain local $path]
-fconfigure $s -translation binary -buffering none -blocking no
-fileevent $s readable [list SockRead $s]
-
-proc tohex int {
-	set out ""
-	foreach c [split $int ""] {
-		append out [format %02x [scan $c %c]]
-	}
-	set out
-}
-
-puts -nonewline $s \0AUTH\r\n
-after 1000 [list \
-	puts -nonewline $s "AUTH EXTERNAL [tohex 1000]\r\n"]
-after 2000 [list \
-	puts -nonewline $s "BEGIN\r\n"]
-
 
 vwait forever
