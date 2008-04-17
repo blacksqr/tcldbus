bccb8693a4116f9c67b34829e9b6df035e5ce537
khomoutov <khomoutov@9d8bde4f-0d4a-0410-8f4e-1563d1b116ea>
2008-04-17 00:29:49 +0000

    Started implementation of [emit], [reply] and related procs. Introduced [MarshalMessage] in src/marshal.tcl (needs implementation).

--- trunk/src/iface.tcl
+++ trunk/src/iface.tcl
@@ -8,6 +8,7 @@ proc ::dbus::invoke {chan object imethod args} {
 	set command ""
 	set ignore 0
 	set noautostart 0
+	set timeout 0
 
 	while {[string match -* [lindex $args 0]]} {
 		set opt [Pop args]
@@ -18,10 +19,12 @@ proc ::dbus::invoke {chan object imethod args} {
 			-command      { set command [Pop args] }
 			-ignoreresult { set ignore 1 }
 			-noautostart  { set noautostart 2 }
+			-timeout      { set timeout [Pop args] }
 			--            { break }
 			default {
 				return -code error "Bad option \"$opt\":\
-					must be one of -destination, -in, -out, -command or -ignoreresult"
+					must be one of -destination, -in, -out, -command, -ignoreresult,\
+					-noautostart or -timeout"
 			}
 		}
 	}
@@ -44,18 +47,18 @@ proc ::dbus::invoke {chan object imethod args} {
 	set flags [expr {$ignore | $noautostart}]
 	set serial [NextSerial $chan]
 
-	foreach chunk [MarshalMethodCall \
-			$flags $serial $dest $object $iface $member $insig $mlist $args] {
+	foreach chunk [MarshalMessage \
+			1 $flags $serial $dest $object $iface $member $insig $mlist $args] {
 		puts -nonewline $chan $chunk
 	}
 
 	if {$ignore} return
 
 	if {$command != ""} {
-		ExpectMethodReply $chan $serial 0 $command
+		ExpectMethodReply $chan $serial $timeout $command
 		return
 	} else {
-		set rvpoint [ExpectMethodReply $chan $serial 0 ""]
+		set rvpoint [ExpectMethodReply $chan $serial $timeout ""]
 		puts "waiting on <$rvpoint>..."
 		vwait $rvpoint
 		puts "Got answer: [set $rvpoint]"
@@ -63,6 +66,115 @@ proc ::dbus::invoke {chan object imethod args} {
 		unset $rvpoint
 		return -code $status -errorcode $code $result
 	}
+}
+
+proc ::dbus::reply {chan replyserial args} {
+	set dest ""
+	set obj ""
+	set sig ""
+	set ignore 0
+	set noautostart 0
+
+	while {[string match -* [lindex $args 0]]} {
+		set opt [Pop args]
+		switch -- $opt {
+			-destination  { set dest [Pop args] }
+			-object       { set obj  [Pop args] }
+			-signature    { set sig  [Pop args] }
+			-ignoreresult { set ignore 1 }
+			-noautostart  { set noautostart 2 }
+			--            { break }
+			default {
+				return -code error "Bad option \"$opt\":\
+					must be one of -destination, -object, -signature,\
+					-ignoreresult or -noautostart"
+			}
+		}
+	}
+
+	if {[catch {SigParseCached $sig} mlist]} {
+		return -code error "Bad signature: $mlist"
+	}
+
+	set flags [expr {$ignore | $noautostart}]
+	set serial [NextSerial $chan]
+
+	# TODO there's no way to pass REPLY_SERIAL to this method...
+	foreach chunk [MarshalMessage \
+			2 $flags $serial $dest $object $iface $member $sig $mlist $args] {
+		puts -nonewline $chan $chunk
+	}
+}
+
+proc ::dbus::emit {chan object imethod args} {
+	set dest ""
+	set sig ""
+	set ignore 0
+	set noautostart 0
+
+	while {[string match -* [lindex $args 0]]} {
+		set opt [Pop args]
+		switch -- $opt {
+			-destination  { set dest [Pop args] }
+			-signature    { set sig  [Pop args] }
+			-ignoreresult { set ignore 1 }
+			-noautostart  { set noautostart 2 }
+			--            { break }
+			default {
+				return -code error "Bad option \"$opt\":\
+					must be one of -destination, -signature, -ignoreresult\
+					or -noautostart"
+			}
+		}
+	}
+
+	if {![SplitMemberName $imethod iface member]} {
+		return -code error "Malformed interfaced method name: \"$imethod\""
+	}
+	if {$iface == ""} {
+		return -code error "No interface name provided"
+	}
+
+	if {[catch {SigParseCached $sig} mlist]} {
+		return -code error "Bad signature: $mlist"
+	}
+
+	set flags [expr {$ignore | $noautostart}]
+	set serial [NextSerial $chan]
+
+	foreach chunk [MarshalMessage \
+			4 $flags $serial $dest $object $iface $member $sig $mlist $args] {
+		puts -nonewline $chan $chunk
+	}
+}
+
+proc ::dbus::trap {chan imethod command args} {
+	set src ""
+	set sig ""
+	set obj ""
+
+	while {[llength $args] > 0} {
+		set opt [Pop args]
+		switch -- $opt {
+			-source    { set src [Pop args] }
+			-signature { set sig [Pop args] }
+			-object    { set obj [Pop args] }
+			default {
+				return -code error "Bad option \"$opt\":\
+					must be one of -source, -signature or -object"
+			}
+		}
+	}
+
+	if {![SplitMemberName $imethod iface member]} {
+		return -code error "Malformed interfaced method name: \"$imethod\""
+	}
+
+	if {[catch {SigParseCached $sig} mlist]} {
+		return -code error "Bad input signature: $mlist"
+	}
+
+	# TODO register the trap
 }
 
 proc ::dbus::remoteproc {name imethod signature args} {
