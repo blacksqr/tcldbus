5aacb1cd8f760b0de81ee795367032cfa63af7b7
khomoutov <khomoutov@9d8bde4f-0d4a-0410-8f4e-1563d1b116ea>
2008-04-30 00:42:47 +0000

    src/client.tcl:  * Fixed processing server address parameters in *Endpoint commands.  * Added [GetUUID] proc for generation of server UUID.  * Fixed naming of some server authentication commands.  * Temporarily commented out retreiving and checking of peerid    for EXTERNAL server auth since ceptcl has grave bugs regarding    support for server sockets and we reverted to using TCP sockets    for testing.
    src/iface.tcl: -server option now accept no argument and has
     the flag semantics.
    
    server.tcl: Commented out UD-sockets code, introduced TCP code
    (see above for reasoning).
    
    NOTE: sample [invoke] causes "stream format error" in the server.

--- server.tcl
+++ server.tcl
@@ -1,11 +1,25 @@
 set auto_path [linsert $auto_path 0 .]
 package require dbus
 
-set server [::dbus::endpoint -server foo unix:path=/tmp/gabbahey]
+if 0 {
+set server [::dbus::endpoint -server unix:path=/tmp/gabbahey]
 puts {Created server}
 
 set client [::dbus::endpoint unix:path=/tmp/gabbahey]
 puts {Created client}
 
 file delete /tmp/gabbahey
+}
+
+set server [::dbus::endpoint -server tcp:host=localhost,port=0]
+set sport  [lindex [fconfigure $server -sockname] 2]
+puts "Created server $server on $sport"
+
+set client [::dbus::endpoint tcp:host=localhost,port=$sport]
+puts "Created client $client"
+
+::dbus::invoke $client /my/cool/object com.googlecode.tcldbus.SomeMethod \
+	-in si -- "Foo bar" 0xAABBCCDD
+
+vwait forever
 
